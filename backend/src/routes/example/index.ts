8a38d6a5d56465b19e0c83aca5bc91246a93f0b8
poyaohuang <poyaohg0705@gmail.com>
2026-06-14 14:41:49 +1000

    Update index.ts

--- backend/src/routes/example/index.ts
+++ backend/src/routes/example/index.ts
@@ -1,9 +1,10 @@
-import { type FastifyPluginAsync } from 'fastify'
+import { type FastifyPluginAsync } from "fastify";
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    return "this is an example";
+    // throw fastify.httpErrors.internalServerError()
+  });
+};
 
-export default example
+export default example;
